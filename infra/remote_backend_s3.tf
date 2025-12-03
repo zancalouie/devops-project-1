a06f7aa3c92afbeed75d452c85a006dc4eb4445a
zancalouie <zancalouie@gmail.com>
2025-12-02 22:02:53 -0500

    init

--- infra/remote_backend_s3.tf
+++ infra/remote_backend_s3.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "dev-proj-1-remote-state-bucket-123456"
     key    = "devops-project-1/terraform.tfstate"
-    region = "eu-central-1"
+    region = "us-east-1"
   }
 }
